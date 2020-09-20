948b8b495aab644f199e1af960faeb8baac6c682
cbowcutt <bowcuttchris@gmail.com>
2020-09-20 10:29:12 -0400

    WIP: unit test for WorkoutCardFragment

---
+++ app/src/test/java/com/example/myfirstapp/fragments/WorkoutCardFragmentTest.java
@@ -0,0 +1,27 @@
+package com.example.myfirstapp.fragments;
+
+import android.os.Bundle;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.fragment.app.FragmentFactory;
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class WorkoutCardFragmentTest {
+    @Test
+    public void workoutCardFragment_create() {
+        fail("pending");
+        Bundle bundle = new Bundle();
+        FragmentScenario.launchInContainer(WorkoutCardFragment.class, bundle);
+    }
+}
